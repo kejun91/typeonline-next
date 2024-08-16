2813daeeadf0c86d7dda2d18fdcc767c3b692014
Jun Ke <jun.ke@wirelesscar.com>
2024-08-16 17:37:08 +0800

    Add some

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This page provides information about our app.</p>
+      </div>
+    );
+  }
+  
---
+++ app/profile/[id]/page.js
@@ -0,0 +1,11 @@
+export default function ProfilePage({ params }) {
+    const { id } = params;
+  
+    return (
+      <div>
+        <h1>Profile Page</h1>
+        <p>Welcome to the profile of user with ID: {id}</p>
+      </div>
+    );
+  }
+  
