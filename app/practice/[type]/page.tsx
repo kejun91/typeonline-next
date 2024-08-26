d8c94d2ca38c02a1c361e12141cc0bb1a2952d92
Jun Ke <anewr@live.com>
2024-08-26 23:40:26 +0800

    Refactor

--- app/practice/[type]/page.tsx
+++ app/practice/[type]/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Practice from "../Practice";
+import MainContent from "@/app/MainContent";
+import InteractiveContent from "../InteractiveContent";
 
 type Props = {
     params: { type: string };
 }
 
 export default function WithType({ params } : Props) {
-    return <Practice pageId={"practice/" + params.type} title={"Copy practice - with " + params.type + "s"} type={params.type} />;
+    return <MainContent pageId={"practice/" + params.type} title={"Copy practice - with " + params.type + "s"} >
+        <InteractiveContent practiceType={params.type} />
+    </MainContent>;
 };
